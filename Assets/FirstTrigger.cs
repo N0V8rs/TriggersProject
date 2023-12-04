7bf9d5bbf60cd65101823712809efa4b4f8daf2f
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2023-12-04 00:19:45 -0400

    Basic
    Adding more triggers that activates lights and jumpscares

--- Assets/FirstTrigger.cs
+++ Assets/FirstTrigger.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class FirstTrigger : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ public class timeLineTrigger : MonoBehaviour
+ {
+     public PlayableDirector Trigger1Start; // public = seeing in the inspector // PlayableDirector = Timeline in Unity for game object // TimeLineTrigger = the name you give it
+     public bool OneTimeTrigger = false; // True if you wanna use the trigger once // Puts it in the inspector
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+     private void OnTriggerEnter(Collider other) // Adds the trigger to enter 
+     {
+         Trigger1Start.Play();// Plays the timeline ( public PlayableDirector "name of the timeline"
+     }
+ }
+
