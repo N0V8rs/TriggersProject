40cea325f8e956d0156fad0bad2e3d30b7559c64
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2023-12-06 18:11:31 -0400

    Updated and polish lights

--- Assets/FirstTrigger.cs
+++ Assets/FirstTrigger.cs
@@ -5,12 +5,12 @@ using UnityEngine.Playables;
 
  public class timeLineTrigger : MonoBehaviour
  {
-     public PlayableDirector Trigger1Start; // public = seeing in the inspector // PlayableDirector = Timeline in Unity for game object // TimeLineTrigger = the name you give it
+    public PlayableDirector Trigger1Start; // public = seeing in the inspector // PlayableDirector = Timeline in Unity for game object // TimeLineTrigger = the name you give it
      public bool OneTimeTrigger = false; // True if you wanna use the trigger once // Puts it in the inspector
 
      private void OnTriggerEnter(Collider other) // Adds the trigger to enter 
      {
-         Trigger1Start.Play();// Plays the timeline ( public PlayableDirector "name of the timeline"
+        Trigger1Start.Play();// Plays the timeline ( public PlayableDirector "name of the timeline"
      }
  }
 
