59fbbe79ae2a54833b1a6678b701014603c2369c
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2023-12-06 21:14:57 -0400

    Finished and added a game over

--- Assets/FirstTrigger.cs
+++ Assets/FirstTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine.Playables;
 
  public class timeLineTrigger : MonoBehaviour
  {
-    public PlayableDirector Trigger1Start; // public = seeing in the inspector // PlayableDirector = Timeline in Unity for game object // TimeLineTrigger = the name you give it
+     public PlayableDirector Trigger1Start; // public = seeing in the inspector // PlayableDirector = Timeline in Unity for game object // TimeLineTrigger = the name you give it
      public bool OneTimeTrigger = false; // True if you wanna use the trigger once // Puts it in the inspector
 
      private void OnTriggerEnter(Collider other) // Adds the trigger to enter 
